cb666aa265927e628b20a88963ff08936bd9dc5d
divyang1998 <divyang1998@gmail.com>
2017-10-16 16:10:55 -0400

    Initial commit

---
+++ app/src/main/java/uottawacom/soccerprofilemanager/ProfileActivity.java
@@ -0,0 +1,44 @@
+package uottawacom.soccerprofilemanager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void SetTeamIcon(View view) {
+//Creating a Return intent to pass to the Main Activity
+        Intent returnIntent = new Intent();
+//Figuring out which image was clicked
+        ImageView selectedImage = (ImageView) view;
+//Adding stuff to the return intent
+        returnIntent.putExtra("imageID", selectedImage.getId());
+        setResult(RESULT_OK, returnIntent);
+//Finishing Activity and return to main screen!
+        finish();
+    }
+
+}
